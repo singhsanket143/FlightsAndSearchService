c570546ab3fcf575789af730dff5c0b4ba130062
Sanket Singh <singhsanket143@gmail.com>
2022-12-13 21:01:24 +0530

    Added city model and migrations

--- README.md
+++ README.md
@@ -21,3 +21,15 @@
 ```
 - Once you've added your db config as listed above, go to the src folder from your terminal and execute `npx sequelize db:create`
 ```
+
+## DB Design
+  - Airplane Table
+  - Flight
+  - Airport
+  - City 
+
+  - A flight belongs to an airplane but one airplane can be used in multiple flights
+  - A city has many airports but one airport belongs to a city
+  - One airport can have many flights, but a flight belongs to one airport
+
+  
---
+++ src/models/city.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class City extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  City.init({
+    name: {
+      type: DataTypes.STRING, 
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'City',
+  });
+  return City;
+};
