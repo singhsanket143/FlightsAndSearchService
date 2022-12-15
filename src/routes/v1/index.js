ef18999160b4850a42b0730b3941fe06082965a4
Sanket Singh <singhsanket143@gmail.com>
2022-12-15 22:02:32 +0530

    Added more routers for city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,8 @@ const CityController = require('../../controllers/city-controller');
 const router = express.Router();
 
 router.post('/city', CityController.create);
+router.delete('/city/:id', CityController.destroy);
+router.get('/city/:id', CityController.get);
+router.patch('/city/:id', CityController.update);
 
 module.exports = router;
