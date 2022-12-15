8e97973f55cfa083d881413659de3e68cc7dcac6
Sanket Singh <singhsanket143@gmail.com>
2022-12-15 20:33:40 +0530

    Completed city repository

--- README.md
+++ README.md
@@ -20,7 +20,11 @@
 
 ```
 - Once you've added your db config as listed above, go to the src folder from your terminal and execute `npx sequelize db:create`
+and then execute
+
+`npx sequelize db:migrate`
 ```
+
 
 ## DB Design
   - Airplane Table
@@ -31,5 +35,6 @@
   - A flight belongs to an airplane but one airplane can be used in multiple flights
   - A city has many airports but one airport belongs to a city
   - One airport can have many flights, but a flight belongs to one airport
+
 
   
--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,11 +1,15 @@
 const { City } = require('../models/index');
 
 class CityRepository {
-    async createCity({ name }) {
+
+    async createCity({ name }) { 
         try {
-            const city = await City.create({ name });
+            const city = await City.create({
+                name
+            });
             return city;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
@@ -17,10 +21,37 @@ class CityRepository {
                     id: cityId
                 }
             });
+            return true;
         } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw {error};
         }
     }
+
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return City;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
 }
 
 module.exports = CityRepository;
